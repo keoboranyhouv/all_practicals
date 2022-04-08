9ea167cb750a71af42a6ab8bbf08e39543e0637c
keoboranyhouv <h.keoborany@gmail.com>
2022-04-08 10:42:43 +0800

    make changes in the subject reader program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,27 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    subject_details = get_data() # retrieve from file to populate subject details list
+    print_subjects(subject_details) # print report using subject_details
+
+
+def print_subjects(subject_details):
+    """Print subject, lecturer and number of students in a nicely formatted report."""
+    for subject_detail in subject_details:
+        print(f"{subject_detail[0]} is taught by {subject_detail[1]:12} and has {subject_detail[2]:3} students")
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject_details = [] # will be a list of lists
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject_details.append(parts) # append individual subject lists to a main list
     input_file.close()
+    return subject_details
 
 
 main()
