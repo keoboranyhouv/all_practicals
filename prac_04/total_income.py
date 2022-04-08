6f911cf6e9c356fede7fb12be69593be64fac4d6
keoboranyhouv <h.keoborany@gmail.com>
2022-04-08 10:15:24 +0800

    make changes in the total income program

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,16 +13,17 @@ def main():
         income = float(input("Enter income for month {}: ".format(str(month))))
         incomes.append(income)
 
-    print_report(incomes, number_of_months)
+    print_report(incomes)
 
 
-def print_report(incomes, number_of_months):
+def print_report(incomes):
+    """Print incomes by month"""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
+    for month in range(0, len(incomes)):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month+1, income, total))
 
 
 main()
