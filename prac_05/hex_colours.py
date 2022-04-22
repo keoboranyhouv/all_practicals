f304ae485ac037b5da186b1e1f1cf31da92936be
keoboranyhouv <h.keoborany@gmail.com>
2022-04-22 10:11:27 +0800

    Completed hex_colours program

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,11 @@
+CODE_TO_COLOUR = {"Absolute Zero": "#0048ba" ,"Acid Green": "#b0bf1a","Aliceblue": "#f0f8ff",
+                  "Alizarin crimson": "#e32636","Amaranth": "#e52b50","Amber": "#ffbf00","Amethyst": "#9966cc",
+                  "AntiqueWhite": "#faebd7","AntiqueWhite1": "#ffefdb","Antiquewhite2": "#eedfcc"}
+
+colour_name = input("Enter Colour: ").title()
+while colour_name != "":
+    if colour_name in CODE_TO_COLOUR:
+        print(CODE_TO_COLOUR[colour_name])
+    else:
+        print("Invalid colour!")
+    colour_name = input("Enter Colour: ").title()
