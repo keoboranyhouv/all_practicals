2584a7dd10884659dee207509338d25e4bfc02e5
keoboranyhouv <h.keoborany@gmail.com>
2022-04-22 11:37:36 +0800

    create extract name from email function

---
+++ prac_05/emails.py
@@ -0,0 +1,38 @@
+def main():
+
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = extract_name_from_email(email)
+        acceptance = input(f'Is your name {name}? (y/n)')
+        if acceptance != "y" and acceptance != " ":
+            name = input('Name: ')
+
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} {email}")
+
+
+def extract_name_from_email(email):
+    designation = email.split('@')[0]
+    parts = designation.split('-')
+    name = ' '.join(parts).title()
+    return name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
