84c092066050ceabbf5b7398df901596204bba13
keoboranyhouv <h.keoborany@gmail.com>
2022-04-29 11:20:43 +0800

    create init function

---
+++ prac_06/guitar.py
@@ -0,0 +1,16 @@
+CURRENT_YEAR = 2022
+VINTAGE_AGE = 50
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+
+
+
+
+
+
