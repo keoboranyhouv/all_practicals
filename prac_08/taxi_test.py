d3f7e3cdc962212a3eb7962bcc24b5719c161c12
keoboranyhouv <h.keoborany@gmail.com>
2022-05-13 09:54:07 +0800

    print current fare

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -5,11 +5,11 @@ def main():
     new_taxi = Taxi('Prius 1', 100)
     new_taxi.drive(40)
     print(new_taxi)
-    print(new_taxi.get_fare())
+    print(f"Current fare: ${new_taxi.get_fare():.2f}")
 
     new_taxi.start_fare()
     new_taxi.drive(100)
     print(new_taxi)
-    print(new_taxi.get_fare())
+    print(f"Current fare: ${new_taxi.get_fare():.2f}")
 
 main()
