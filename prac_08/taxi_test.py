9131ce01832dd75abb22787e94261b35632d8e39
keoboranyhouv <h.keoborany@gmail.com>
2022-05-13 09:36:20 +0800

    restart the taxi meter

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -7,6 +7,9 @@ def main():
     print(new_taxi)
     print(new_taxi.get_fare())
 
-
+    new_taxi.start_fare()
+    new_taxi.drive(100)
+    print(new_taxi)
+    print(new_taxi.get_fare())
 
 main()
