b0b3306d875fef071fd7326dd67193e0e23c3732
keoboranyhouv <h.keoborany@gmail.com>
2022-05-13 13:08:05 +0800

    add display taxi function

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,30 @@
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+MENU = 'q)uit, c)hoose taxi, d)rive'
+taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+
+
+def main():
+    current_taxi = None
+    print("let's drive!")
+    print(MENU)
+    current_menu = input('>>> ').lower()
+    while current_menu != "":
+        if current_menu == 'c':
+            print('Taxis available: ')
+            display_taxi(taxis)
+
+
+
+
+    pass
+
+
+def display_taxi(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f'{i} - {taxi}')
+
+
+if __name__ == '__main__':
+    main()
